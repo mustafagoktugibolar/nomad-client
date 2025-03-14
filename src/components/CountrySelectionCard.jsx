3a0b08b6fe826808b820402b83e3f3b483c58330
Mustafa Göktuğ İbolar <goktugibolar@icloud.com>
2025-03-15 01:42:26 +0300

    Made changes before updating to tsx

--- src/components/CountrySelectionCard.jsx
+++ src/components/CountrySelectionCard.jsx
@@ -7,9 +7,16 @@ const CountrySelectionCard = ({ countries, onSelectCountry }) => {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <Card className="p-3 h-100">
+    <Card
+      className="pt-3 h-100 border-0 d-flex flex-column"
+      style={{ paddingBottom: "30px" }}
+    >
       <CountrySearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <CountryList countries={countries} searchTerm={searchTerm} onSelectCountry={onSelectCountry} />
+      <CountryList
+        countries={countries}
+        searchTerm={searchTerm}
+        onSelectCountry={onSelectCountry}
+      />
     </Card>
   );
 };
